75cfc75c7a8e12fb610f6caec6d7dfc49cc27708
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-09-23 20:09:14 +0200

    <3 #2

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int wowko;
+            Int32.TryParse(input, out wowko);
+            if (wowko < 10)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
     }
 }
